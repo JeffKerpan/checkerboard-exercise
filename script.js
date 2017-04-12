7f0c220c6935e2b7816f8f4c6befe7804c5e3202
Jeff Kerpan <jkerpan@gmail.com>
2017-04-12 16:51:59 -0600

    Added random colors

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ var totalCols = 8;
 
 for (var row = 1; row <= totalRows; row++) {
   for (var col = 1; col <= totalCols; col++){
-    var color = isBlack(row,col) ? 'black' : 'red';
+    // RGB - Red Green Blue - 0-255
+    var red = getRandomArbitrary(0, 255);
+    var green = getRandomArbitrary(0, 255);
+    var blue = getRandomArbitrary(0, 255);
+    var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
     body.appendChild(genTile(color));
   }
 }
 function genTile(color) {
   var width = (100 / totalCols) + '%';
   var tile = document.createElement('div');
-  tile.style.width = width
+  tile.style.width = width;
   tile.style.float = 'left';
   tile.style.paddingBottom = width;
   tile.style.backgroundColor = color;
   return tile;
 }
 
-function isBlack(row,col) {
-  return (row % 2 === 0 && col % 2 === 0) ||
-         (row % 2 === 1 && col % 2 === 1)
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// function isBlack(row,col) {
+//   return (row % 2 === 0 && col % 2 === 0) ||
+//          (row % 2 === 1 && col % 2 === 1)
+// }
